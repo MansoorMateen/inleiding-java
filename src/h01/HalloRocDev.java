0da7e46195a4477f201922853c30b47d2f2199f3
MansoorMateen <2047555@talnet.nl>
2020-10-19 17:20:47 +0200

    succesvol veranderd

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas 8A!");
+        System.out.println("we will die if we don't do anything against climate change!");
     }
 }
