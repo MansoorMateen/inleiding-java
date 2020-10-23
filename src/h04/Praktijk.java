3939c4f0aee1bba7ff4bcaddc5a97ff1bd716c94
MansoorMateen <2047555@talnet.nl>
2020-10-23 09:58:14 +0200

    werkt 2

---
+++ src/h04/Praktijk.java
@@ -0,0 +1,43 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+
+//Teken de zijde waarop de vier van een dobbelsteen staat
+
+
+import java.awt.*;
+import java.applet.*;
+
+public class Praktijk extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        //text
+        g.drawString("Lijn",105,30);
+        g.drawString("Rechthoek",90,155);
+        g.drawString("Afgeronde Rechthoek",60,280);
+        g.drawString("Gevulde Rechthoek Met Ovaal",250,155);
+        g.drawString("Gevulde Ovaal",300,280);
+        g.drawString("Taartpunt Met Ovaal Eromheen",500,155);
+        g.drawString("Cirkel",560,280);
+
+        //vormen
+        g.drawLine(10,10,210,10);
+        g.drawRect(10,40,200,100);
+        g.drawRoundRect(10,160,200,100,30,30);
+        g.setColor(Color.magenta);
+        g.fillRect(250,40,200,100);
+        g.fillArc(250,160,200,100,0,360);
+        g.fillArc(490,40,200,100,0,45);
+        g.setColor(Color.black);
+        g.drawArc(250,40,200,100,0,360);
+        g.drawArc(490,40,200,100,0,360);
+        g.drawArc(530,160,100,100,0,360);
+
+
+    }
+}
--- src/h04/h047.java
+++ src/h04/h047.java
@@ -1,6 +1,7 @@
 package h04;
 
-//Teken met fillArc() op een blauwe achtergrond een ellips die met geel is gevuld.
+//Teken de zijde waarop de vier van een dobbelsteen staat
+
 
 import java.awt.*;
 import java.applet.*;
@@ -12,6 +13,9 @@ public class h047 extends Applet {
 
     public void paint(Graphics g) {
        g.drawRoundRect(40,40,100,100,20,20);
-
+       g.fillArc(50,50,20,20,360,360);
+       g.fillArc(50,110,20,20,360,360);
+       g.fillArc(110,50,20,20,360,360);
+       g.fillArc(110,110,20,20,360,360);
     }
 }
